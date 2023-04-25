e2030571653f3618bc89d34e6aef00b3fa9fa280
Taehoon Kim <enter4652@naver.com>
2023-04-25 17:56:55 +0900

    4/25

---
+++ 등차수열의 특정한 향만 더하기.py
@@ -0,0 +1,22 @@
+'''두 정수 a, d와 길이가 n인 boolean 배열 included가 주어집니다.
+첫째항이 a, 공차가 d인 등차수열에서 included[i]가 i + 1항을 의미할 때,
+이 등차수열의 1항부터 n항까지 included가 true인 항들만 더한 값을 return 하는 solution 함수를 작성해 주세요.'''
+
+
+# 나의 풀이
+def solution(a, d, included):
+    answer = 0 
+    for i in range(len(included)):
+        if included[i] == True:
+            answer += a
+        a += d
+    return answer
+
+
+# 다른 사람 풀이
+def solution(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        answer += (a + d * i) * int(included[i])
+        print(answer)
+    return answer
