a0e513fb91b9319ae7621970e1983a629ab2aa57
Taehoon Kim <enter4652@naver.com>
2023-04-26 17:55:47 +0900

    4/26

---
+++ 세로 읽기.py
@@ -0,0 +1,13 @@
+'''문자열 my_string과 두 정수 m, c가 주어집니다.
+my_string을 한 줄에 m 글자씩 가로로 적었을 때,
+왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.'''
+
+
+# 나의 풀이
+def solution(my_string, m, c):
+    return ''.join([my_string[i] for i in range(c-1, len(my_string), m)])
+
+
+# 다른 사람 풀이
+def solution(s, m, c):
+    return s[c-1::m]
