e0a0b7fe3bac04dac115583dfb4af1a4a772a293
Taehoon Kim <enter4652@naver.com>
2023-06-09 17:59:48 +0900

    6/9

---
+++ 이상한 문자 만들기.py
@@ -0,0 +1,18 @@
+'''문자열 s는 한 개 이상의 단어로 구성되어 있습니다.
+각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+각 단어의 짝수번째 알파벳은 대문자로,
+홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.'''
+
+
+# 나의 풀이
+def solution(s):
+    s = s.split(" ")
+    answer = ""
+    for i in s:
+        for j in range(len(i)):
+            if j%2:
+                answer += i[j].lower()
+            else:
+                answer += i[j].upper()
+        answer += " "
+    return answer[:-1]
